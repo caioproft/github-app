5fcad25b27907050ac49fd723f530b201c4b36fb
Caio Silveira Fermiano de Proft <caio-proft@caio.proft>
2020-03-04 16:05:23 -0300

    Adicionando repositórios e listando

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -32,10 +32,19 @@ export const Form = styled.form`
     font-size: 16px;
   }
 `;
+const rotate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+  }
+`
 
-export const SubmitButton = styled.button.attrs({
+export const SubmitButton = styled.button.attrs( props =>({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -45,4 +54,37 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   align-items: center;
   justify-content: center;
+
+  &[disabled]{
+    cursor: not-allowed;
+    opacity: 0.7;
+  }
+
+  ${ props => props.loading && css`
+  svg{
+    animation:${rotate} 2s linear infinite;
+  }` }
 `;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li{
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    & + li{
+      border-top: 1px solid #eee;
+    }
+
+    a{
+      text-decoration: none;
+      color: #7159c1;
+    }
+  }
+
+`
