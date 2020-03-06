113d0baf90f852c0ce2da34ff1b906ea0fbce288
Caio Proft <caio_proft@hotmail.com>
2020-03-06 13:52:34 -0300

    :construction: Criando a rota para detalhes de repositórios

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Repository(){
+export default function Repository({ match }){
   return(
-    <h1>Repositórios</h1>
+  <h1>Repositórios: {decodeURIComponent(match.params.repository)} </h1>
   )
 }
